f7487c615007ef3dcf4af2a3692f845d7bfc9161
mirza <mirza.akhena@gmail.com>
2024-07-10 17:54:37 +0700

    update serverless api

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 // src/index.ts
 import express from "express";
-import { APIGatewayProxyHandler, APIGatewayEvent, Context } from "aws-lambda";
 import serverless from "serverless-http";
 
 const app = express();
@@ -13,9 +12,13 @@ app.get("/api/time", (req, res) => {
   res.json({ currentTime: new Date().toISOString() });
 });
 
-const serverlessHandler = serverless(app);
+const handler = serverless(app);
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayEvent, context: Context) => {
-  const result = await serverlessHandler(event, context);
-  return result as any;
-};
+export { handler };
+
+// const serverlessHandler = serverless(app);
+
+// export const handler: APIGatewayProxyHandler = async (event: APIGatewayEvent, context: Context) => {
+//   const result = await serverlessHandler(event, context);
+//   return result as any;
+// };
